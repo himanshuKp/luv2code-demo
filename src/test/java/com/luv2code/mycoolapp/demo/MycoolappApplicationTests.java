64d61048a42f5486ffafccdea82cbb140b57c4aa
Himanshu Kandpal <himanshukp324@gmail.com>
2023-04-02 05:47:47 +0530

    basic structure

---
+++ src/test/java/com/luv2code/mycoolapp/demo/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.mycoolapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
